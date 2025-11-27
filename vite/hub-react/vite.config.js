c360680868f6ba83a910ef2e6626e929e3259d87
DrxCode-tech <anietiedaniel11@gmail.com>
2025-11-27 14:17:25 +0100

    rhrhf

--- vite/hub-react/vite.config.js
+++ vite/hub-react/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",       // Required for Vercel
-  build: {
-    outDir: "dist" // This ensures the build goes into hub-react/dist
-  }
 });
